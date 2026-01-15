e5a126743683dc3591316f4069137bc4ae821bda
Paulo Reis <pauloreisprofissional@gmail.com>
2026-01-14 21:25:37 -0300

    Exemplo básico de configuração do Router

--- public/index.php
+++ public/index.php
@@ -1,8 +1,25 @@
-<?php
+<?php declare(strict_types=1);
 
-use App\Controllers\HelloController;
+use Laminas\Diactoros\ServerRequestFactory;
+use League\Route\Router;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Laminas\Diactoros\Response;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$helloController = new HelloController();
-$helloController->dizerOi();
+$request = ServerRequestFactory::fromGlobals(
+    $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+);
+
+$router = new Router();
+
+$router->map('GET', '/', function (ServerRequestInterface $request): ResponseInterface {
+    $response = new Response;
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+    return $response;
+});
+
+$response = $router->dispatch($request);
+
+(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
